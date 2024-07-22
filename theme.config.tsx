63fc4a0846dc498d144a6bd253ee778c9249a28d
Jinhyung Lee <luciancah@gmail.com>
2024-07-22 16:06:44 +0900

    앱에 라우팅

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
+import AppOnly from "components/apponly";
+import PagesOnly from "components/pagesonly";
+import Cross from "components/cross";
+import Check from "components/check";
 
 const config: DocsThemeConfig = {
   logo: <span>Nextjs 한국어 번역</span>,
@@ -23,6 +27,12 @@ const config: DocsThemeConfig = {
         🎉 공식문서 번역에 참여해주세요.
       </a>
     ),
+  },
+  components: {
+    Check,
+    Cross,
+    AppOnly,
+    PagesOnly,
   },
   editLink: {
     component: null,
